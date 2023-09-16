3e8f1e5b79c23ac422e0b6ae084b16a4cb65ca50
Vinayak Jaiswal <96956957+Vinayak1802@users.noreply.github.com>
2023-09-16 10:55:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
-const sendChatBtn=document.querySelector(".chat-input i");
-const chatInput=document.querySelector(".chat-input textarea")
-const chatbox=document.querySelector(".chatbox")
-
-
-
-let userMessage;
-
-const API_KEY="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";
-
-const createChatLi=(message,className)=>{//Create a chat <li> element with passed message and className
-    const chatLi=document.createElement("li")
-    chatLi.classList.add("chat",className);
-    let chatContent=className==="outgoing"?`<p>${message}</p>`:`<i class='material-icons'>smart_toy</i><p><p>${message}</p>`
-    chatLi.innerHTML=chatContent;
-    return chatLi;
-}
-
-const generateResponse=(incomingChatLi)=>{
-
-
-    const API_URL="https://api.openai.com/v1/chat/completions";
-    const messageElement=incomingChatLi.querySelector("p");
-
-    const requestOptions={//this is only for converting data in json file so that we can easily fetch the content from the API data
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json",
-            "Authorization":`Bearer ${API_KEY}`
-        },
-        body:JSON.stringify({
-            model: "gpt-3.5-turbo-0613",
-            messages:
-            [{role: "user",content:userMessage}]
-
-        })
-    }
-
-    fetch(API_URL,requestOptions).then(res=>res.json()).then(data=>{
-        messageElement.textContent=data.choices[0].message.content;
-        console.log(data)
-    }).catch((err)=>{
-        messageElement.textContent="Oops! Something went wrong.. ";    })
-}
-
-const handleChat=()=>{
-    userMessage=chatInput.value.trim();
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.group');
     
-    if(!userMessage) return;
-    chatbox.appendChild(createChatLi(userMessage,"outgoing"));//this is for handling the chat in layout and append the user's message to the chatbot 
+    dropdowns.forEach(dropdown => {
+        dropdown.addEventListener('mouseenter', function() {
+            this.querySelector('ul').classList.remove('hidden');
+        });
+        
+        dropdown.addEventListener('mouseleave', function() {
+            this.querySelector('ul').classList.add('hidden');
+        });
+    });
+});
 
-    // chatbox.scrollTo(0,chatbox.scrollHeight);
 
-    setTimeout(()=>{
-        const incomingChatLi=createChatLi("","incoming")
-        chatbox.appendChild(incomingChatLi);
-        generateResponse(incomingChatLi);
-    },600);
+document.addEventListener("DOMContentLoaded", function () {
+    const openSidebarButton = document.getElementById("openSidebar");
+    const sidebar = document.getElementById("sidebar");
 
-}
-sendChatBtn.addEventListener("click",handleChat);
+    openSidebarButton.addEventListener("click", function () {
+        sidebar.classList.toggle("-translate-x-full");
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownToggle = document.querySelector('.relative');
+  
+    dropdownToggle.addEventListener('click', function() {
+      const dropdownMenu = this.querySelector('ul');
+      dropdownMenu.classList.toggle('hidden');
+    });
+  
+    // Close dropdown when clicking outside of it
+    document.addEventListener('click', function(event) {
+      const dropdownMenu = dropdownToggle.querySelector('ul');
+      if (event.target !== dropdownToggle && !dropdownToggle.contains(event.target)) {
+        dropdownMenu.classList.add('hidden');
+      }
+    });
+  });
+
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const companyDropdown = document.querySelector('.relative');
+
+    companyDropdown.addEventListener('mouseenter', function () {
+      const dropdown = this.querySelector('ul');
+      dropdown.classList.remove('hidden');
+    });
+
+    companyDropdown.addEventListener('mouseleave', function () {
+      const dropdown = this.querySelector('ul');
+      dropdown.classList.add('hidden');
+    });
+  });
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const companyDropdown = document.getElementById('companyDropdown');
+    const dropdownList = companyDropdown.querySelector('ul');
+
+    companyDropdown.addEventListener('mouseenter', () => {
+      dropdownList.classList.remove('hidden');
+    });
+
+    companyDropdown.addEventListener('mouseleave', () => {
+      dropdownList.classList.add('hidden');
+    });
+  });
